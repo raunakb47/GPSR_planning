5bb05db4442e33c2144f8df3bc310f032e7b2f22
Miguel Ángel González Santamarta <mgons@unileon.es>
2024-03-18 22:15:34 +0100

    string fixes

--- gpsr_planning/gpsr_planning/gpsr_planning_node.py
+++ gpsr_planning/gpsr_planning/gpsr_planning_node.py
@@ -4,58 +4,89 @@ from rclpy.node import Node
 from rclpy.action import ActionClient
 from gpsr_msgs.srv import ExecutePlan
 from rclpy.callback_groups import ReentrantCallbackGroup
-from rclpy.executors import MultiThreadedExecutor, Executor
+from rclpy.executors import MultiThreadedExecutor
 from llama_msgs.action import GenerateResponse
+
 
 class GPSRPlanning(Node):
 
     def __init__(self):
-        super().__init__('planning_node')
+        super().__init__("planning_node")
 
-        count_bt_node = '''count: Robot action to count the objects in the room, args json schema: {\n'object_type': {\n'title': 'Type of the object to be counted',\n'type': 'string'\n}, \n}'''
+        count_bt_node = """count: count the objects in the room, args json schema: {'object_type': 'Type of the object to be counted'}"""
 
-        describeO_bt_node = '''describe_object: Robot action to describe an object, args json schema: {\n'object': {\n'title': 'Object to be described'\n'type': 'string'\n}, \n}'''
+        describeO_bt_node = """describe_object: describe an object, args json schema: {'object': 'Object to be described'}"""
 
-        describeP_node = '''describe_person: Robot action to describe a person, args json schema: {\n'object': {\n'title': 'Person to be described'\n'type': 'string'\n}, \n}'''
+        describeP_node = """describe_person: describe a person, args json schema: {'person': 'Person to be described'}"""
 
-        findO_bt_node = '''find_object: Robot action to find an object in the room, args json schema: {\n'object': {\n'title': 'Object to be found'\n'type': 'string'\n}, \n}'''
+        findO_bt_node = """find_object: find an object in the room, args json schema: {'object': 'Object to be found'}"""
 
-        findP_bt_node = '''find_person: Robot action to find a person in the room, args json schema: {\n'person': {\n'title': 'Person to be found'\n'type': 'string'\n}, \n}'''
+        findP_bt_node = """find_person: find a person in the room, args json schema: {'person': 'Person to be found'}"""
 
-        followP_bt_node = '''follow_person: Robot action to follow a person, args json schema: {\n'person': {\n'title': 'Object to be followed'\n'type': 'string'\n}, \n}''' 
+        followP_bt_node = """follow_person: follow a person, args json schema: {'person': 'Person to be followed'}"""
 
-        listen_bt_node = '''listen: Robot action to listen and transcribe what a person tells to the robot, args json schema: {\n \n}''' 
+        listen_bt_node = """listen: listen and transcribe what a person tells to the robot, args json schema: { }"""
 
-        lookAround_bt_node = '''look_around: Robot action to turn its head and look around for something, args json schema: {\n \n}'''      
-        
-        lookTo_bt_node = '''look_to: Robot action to make the robot look at the person, args json schema: {\n'person': {\n'title': 'Person to be looked'\n'type': 'string'\n}, \n}'''
+        lookAround_bt_node = """look_around: turn its head and look around for something, args json schema: { }"""
 
-        moveTo_bt_node = '''move_to: Robot action to move from the waypoint where it is to another waypoint, args json schema: {\n'target_waypoint': {\n'title': 'Target Waypoint'\n'type': 'string'\n}, \n}'''
+        lookTo_bt_node = """look_to: make the robot look at the person, args json schema: {'person': 'Person to be looked'}"""
 
-        guide_bt_node = '''guide_person: Robot action to guide a person to a waypoint, args json schema: {\n'target_waypoint': {\n'title': 'Target Waypoint'\n'type': 'string'\n}, 'person': {\n'title': 'Person to guide'\n'type': 'string'\n} \n}'''
+        moveTo_bt_node = """move_to: move the robot to another waypoint, args json schema: {'target_waypoint': 'Target Waypoint'}"""
 
-        offer_bt_node = '''offer: Robot action to offer an object to a person, args json schema: {\n'object': {\n'title': 'Object to be offered'\n'type': 'string'\n}, \n}'''
+        guide_bt_node = """guide_person: guide a person to a waypoint, args json schema: {'target_waypoint': 'Target Waypoint', 'person': 'Person to guide'}"""
 
-        pickO_bt_node = '''pick_object: Robot action to pick and object in the location, args json schema: {\n'object': {\n'title': 'Object to be picked'\n'type': 'string'\n}, \n}'''
+        offer_bt_node = """offer: offer an object to a person, args json schema: {'object': 'Object to be offered'}"""
 
-        placeO_bt_node = '''place_object: Robot action to place and object in the location, args json schema: {\n'object': {\n'title': 'Object to be placed'\n'type': 'string'\n}, \n}'''
+        pickO_bt_node = """pick_object: pick and object in the location, args json schema: {'object': 'Object to be picked'}"""
 
-        query_bt_node = '''query_object: Robot action to obtain information about what a person has told it, args json schema: {\n'text': {\n'title': 'Text to be processed'\n'type': 'string'\n}, \n}'''
+        placeO_bt_node = """place_object: place and object in the location, args json schema: {'object': 'Object to be placed'}"""
 
-        recognizeP_bt_node = '''recognize_person: Robot action to recognize a person after having met her, args json schema: {\n'person': {\n'title': 'Person to be recognized'\n'type': 'string'\n}, \n}'''
+        query_bt_node = """query_object: obtain information about what a person has told it, args json schema: {'text': 'Text to be processed'}"""
 
-        speak_bt_node = '''speak: Robot action to speak something to a person, args json schema: {\n'text': {\n'title': 'Text to tell the person'\n'type': 'string'\n}, \n}'''
+        recognizeP_bt_node = """recognize_person: recognize a person after having met her, args json schema: {person': 'Person to be recognized'}"""
 
-        bt_node_list = [count_bt_node, describeO_bt_node, describeP_node, followP_bt_node, findO_bt_node, findP_bt_node, listen_bt_node, guide_bt_node, lookAround_bt_node, lookTo_bt_node, moveTo_bt_node, offer_bt_node, pickO_bt_node, placeO_bt_node, query_bt_node, recognizeP_bt_node, speak_bt_node]
-        self._bt_nodes = ', '.join(bt_node_list)
-        self._prompt = "You are an AI planner for a robotic assistant named TIAGo. You have to generate plans, which are sequences of actions to achieve goals. Plans may contain no actions, one action, or several actions. Use only the actions listed below. If the goal is already achieved in the world state, return an empty sequence of actions. Use the fewest actions to generate the plan. You have to response in JSON format.\n\n"
+        speak_bt_node = """speak: speak something to a person, args json schema: {'text': 'Text to tell the person'}"""
+
+        bt_node_list = [
+            count_bt_node,
+            describeO_bt_node,
+            describeP_node,
+            followP_bt_node,
+            findO_bt_node,
+            findP_bt_node,
+            listen_bt_node,
+            guide_bt_node,
+            lookAround_bt_node,
+            lookTo_bt_node,
+            moveTo_bt_node,
+            offer_bt_node,
+            pickO_bt_node,
+            placeO_bt_node,
+            query_bt_node,
+            recognizeP_bt_node,
+            speak_bt_node
+        ]
+
+        self._bt_nodes = ""
+        for bt in bt_node_list:
+            self._bt_nodes += f"- {bt}\n"
+
+        self._prompt = """You are an AI planner for a robotic assistant named TIAGo. 
+You have to generate plans, which are sequences of actions to achieve a goal. 
+Plans may contain no actions, one action, or several actions. 
+Use only the actions listed below. 
+Make plans as short as you can. 
+If the goal is already achieved in the world state, return an empty sequence of actions. 
+Reason about the goal, the plan and the actions. 
+You have to response in JSON format.\n\n"""
+
         self._grammar = json.dumps({
-                "type": "object",
-                "properties": {
+            "type": "object",
+            "properties": {
                     "reasoning": {
                         "type": "string"
                     },
-                    "actions": {
+                "actions": {
                         "type": "array",
                         "items": {
                             "type": "object",
@@ -70,50 +101,52 @@ class GPSRPlanning(Node):
                         },
                         "minItems": 0,
                         "maxItems": -1
-                    }
-                },
-                "required": ["team_name"],
-            })
+                }
+            }
+        })
 
         self._cb_group = ReentrantCallbackGroup()
-        self._srv = self.create_service(ExecutePlan, 'gpsr_planning', callback=self._execute_cb, callback_group= ReentrantCallbackGroup())
-        self._action_client = ActionClient(self, GenerateResponse, "/llama/generate_response", callback_group= ReentrantCallbackGroup())
+        self._srv = self.create_service(
+            ExecutePlan, "gpsr_planning", callback=self._execute_cb, callback_group=ReentrantCallbackGroup())
+        self._action_client = ActionClient(
+            self, GenerateResponse, "/llama/generate_response", callback_group=ReentrantCallbackGroup())
         self._action_client.wait_for_server()
-        
-    def _execute_cb(self, request, result):
-        self.get_logger().info(f'Executing plan with the following command: {request.command}')
 
-        self._prompt = self._prompt + "ACTIONS:\n" + self._bt_nodes + "\n\n" + "GOAL: "+ request.command + "\n\n" + "### Instruction:\nGenerate a plan to achieve the goal.\n\n### Response:\n"
-        
-        self.get_logger().info(f'prompt: {self._prompt}')
+    def _execute_cb(self, request: ExecutePlan.Request, response: ExecutePlan.Response) -> ExecutePlan.Response:
+        self.get_logger().info(
+            f"Executing plan with the following command: {request.command}")
+
+        prompt = self._prompt + "ACTIONS:\n" + self._bt_nodes + "\n\n" + "GOAL: " + request.command + \
+            "\n\n" + "### Instruction:\nGenerate a plan, a sequence of actions, to fully achieve the goal.\n\n### Response:\n"
+
+        self.get_logger().info(f"prompt: {prompt}")
 
         goal_msg = GenerateResponse.Goal()
-        goal_msg.prompt = self._prompt
+        goal_msg.prompt = prompt
         goal_msg.reset = True
         goal_msg.sampling_config.temp = 0.0
-        goal_msg.sampling_config.gramar_schema = self._grammar
-        goal_msg.sampling_config.prop_order =["reasoning", "actions", "action_name", "action_args"]
+        goal_msg.sampling_config.grammar_schema = self._grammar
+        goal_msg.sampling_config.prop_order = [
+            "reasoning", "actions", "action_name", "action_args"]
 
+        self.get_logger().info("manda el goal")
 
-        self.get_logger().info(f'manda el goal')
+        result: GenerateResponse.Result = self._action_client.send_goal(
+            goal_msg).result
+        self.get_logger().info(f"{result.response.text}")
 
-        result = self._action_client.send_goal(goal_msg)
-        self.get_logger().info(f'{result.result.response.text}')
+        self.get_logger().info("lo mando")
 
-        self.get_logger().info(f'lo mando')
+        response.success = True
 
+        return response
 
-
-        result.success = True
-
-        return result
 
 def main(args=None):
     rclpy.init(args=args)
     node = GPSRPlanning()
     executor = MultiThreadedExecutor()
     executor.add_node(node)
-    
     executor.spin()
     executor.shutdown()
 
